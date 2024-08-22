ee14af0098512e38e642ab43f039a2a923c0a5dd
Sai Kiran <neelakantamsaikiran7@gmail.com>
2024-08-22 19:18:32 +0530

    init and db connection

---
+++ prisma/migrations/20240822134620_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "Schools" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Schools_pkey" PRIMARY KEY ("id")
+);
